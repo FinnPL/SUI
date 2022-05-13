70c14dd9429c8d28e1b3a3ae04d9f773be610b2b
finnp <finnp@F>
2022-05-13 22:19:25 +0200

    Projektdateien hinzufügen.

---
+++ Test-Projekt/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Test_Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.auth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.UseSystemPasswordChar = true;
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // auth
+            // 
+            resources.ApplyResources(this.auth, "auth");
+            this.auth.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.auth.Name = "auth";
+            this.auth.UseVisualStyleBackColor = true;
+            this.auth.Click += new System.EventHandler(this.auth_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Controls.Add(this.auth);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button auth;
+    }
+}
+
---
+++ Test-Projekt/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Projekt
+{
+    public partial class Form1 : Form
+    {
+        String x;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+
+            DialogResult dr = MessageBox.Show("SUI?", "Bist du ein Süer?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dr == DialogResult.Yes)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"sui.wav");
+                player.Play();
+                DialogResult s = MessageBox.Show("SUI", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+              
+                while ( s == DialogResult.OK)
+                {
+                    player.Play();
+                    s = MessageBox.Show("SUI", "", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                    
+                }
+                
+               
+                
+            } else {
+                this.Close();    
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void auth_Click(object sender, EventArgs e)
+        {
+            if (x == "sui")
+            {
+                button2.Enabled = true;
+                MessageBox.Show("SUI", "Auth richtig");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+         x=textBox1.Text;
+            
+        }
+    }
+}
